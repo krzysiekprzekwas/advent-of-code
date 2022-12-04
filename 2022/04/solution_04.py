7464061074c3d4a8f3e710bba22178bee58b5164
przekwaskrzysiek <przekwaskrzysiek@gmail.com>
2022-12-04 10:33:58 +0100

    Slight refactor

--- 2022/04/solution_04.py
+++ 2022/04/solution_04.py
@@ -1,8 +1,8 @@
 def range_fully_contains(a1, b1, a2, b2):
-    return int(a2) >= int(a1) and int(b2) <= int(b1)
+    return a2 >= a1 and b2 <= b1
 
 def range_overlaps(a1, b1, a2, b2):
-    return max(int(a1),int(a2)) <= min(int(b1),int(b2))
+    return max(a1,a2) <= min(b1,b2)
 
 with open("input_04.txt", "r") as f:
     file_data = f.read().splitlines()
@@ -12,14 +12,15 @@ with open("input_04.txt", "r") as f:
     for pair in file_data:
         elf_1, elf_2 = pair.split(',')
 
-        elf_1_min, elf_1_max = elf_1.split('-')
-        elf_2_min, elf_2_max = elf_2.split('-')
+        elf_1_min, elf_1_max = map(int, elf_1.split('-'))
+        elf_2_min, elf_2_max = map(int, elf_2.split('-'))
 
-        if range_fully_contains(elf_1_min, elf_1_max, elf_2_min, elf_2_max) or range_fully_contains(elf_2_min, elf_2_max, elf_1_min, elf_1_max):
-            score_1 += 1
-
-        if range_overlaps(elf_1_min, elf_1_max, elf_2_min, elf_2_max) or range_overlaps(elf_2_min, elf_2_max, elf_1_min, elf_1_max):
+        if range_overlaps(elf_2_min, elf_2_max, elf_1_min, elf_1_max):
             score_2 += 1
+            
+            if range_fully_contains(elf_1_min, elf_1_max, elf_2_min, elf_2_max) or range_fully_contains(elf_2_min, elf_2_max, elf_1_min, elf_1_max):
+                score_1 += 1
+
 
 print(score_1)
 print(score_2)
